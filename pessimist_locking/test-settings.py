ece2d37109cbe1fa90f3648e73e1209b6f0038ef
rogue707dev <rogue901126@protonmail.com>
2019-10-07 18:23:39 +0200

    added django_sonar

--- pessimist_locking/test-settings.py
+++ pessimist_locking/test-settings.py
@@ -8,6 +8,13 @@
 #     Copyright (c) 2019. All rights reserved.
 #
 ################################################################
+import os
+
+
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+
+SONAR_SCANNER_CMD = '/usr/local/sonar-scanner-cli/bin/sonar-scanner'
+SONAR_COVERAGE_SOURCE_TO_BASE_DIR = '/home/ck/workspace-python/django-pessimist_locking'
 
 
 INSTALLED_APPS = [
@@ -15,7 +22,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.auth',
     'django.contrib.contenttypes',
+    'django.contrib.messages',
     'pessimist_locking',
+    'sonar',
 ]
 
 
@@ -32,8 +41,32 @@ DATABASES = {
 
 
 MIDDLEWARE = [
+    'django.middleware.security.SecurityMiddleware',
+    'django.contrib.sessions.middleware.SessionMiddleware',
+    'django.middleware.common.CommonMiddleware',
+    'django.middleware.csrf.CsrfViewMiddleware',
+    'django.contrib.auth.middleware.AuthenticationMiddleware',
+    'django.contrib.messages.middleware.MessageMiddleware',
+    'django.middleware.clickjacking.XFrameOptionsMiddleware',
     'pessimist_locking.middleware.SoftPessimisticLockReleaseMiddleware',
 ]
 
 
 SECRET_KEY = 'secret-key'
+
+
+TEMPLATES = [
+    {
+        'BACKEND': 'django.template.backends.django.DjangoTemplates',
+        'DIRS': [],
+        'APP_DIRS': True,
+        'OPTIONS': {
+            'context_processors': [
+                'django.template.context_processors.debug',
+                'django.template.context_processors.request',
+                'django.contrib.auth.context_processors.auth',
+                'django.contrib.messages.context_processors.messages',
+            ],
+        },
+    },
+]
